77b64ca3495a9e434de04629e1790c12d005c00d
mason <masonlee7629@gmail.com>
2022-12-18 02:41:35 +0900

    [Fix] implemented mapper

--- to-do-app/src/main/java/com/codestates/todoapp/mapper/TodoMapper.java
+++ to-do-app/src/main/java/com/codestates/todoapp/mapper/TodoMapper.java
@@ -1,7 +1,12 @@
 package com.codestates.todoapp.mapper;
 
+import com.codestates.todoapp.dto.TodoDto;
+import com.codestates.todoapp.entity.Todos;
 import org.mapstruct.Mapper;
 
-@Mapper
-public class TodoMapper {
+@Mapper(componentModel = "spring")
+public interface TodoMapper {
+    Todos postDtoToTodo(TodoDto.Post requestBody);
+    Todos patchDtoToTodo(TodoDto.Patch requestBody);
+    TodoDto.Response todoToResponseDto(Todos todos);
 }
