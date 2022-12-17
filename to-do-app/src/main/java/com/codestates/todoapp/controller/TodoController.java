21086e9a8e15c46ccf187257fd9cb3c5adc0c92a
mason <masonlee7629@gmail.com>
2022-12-18 02:59:07 +0900

    [Fix] Add parameter of handlerMethod in controller

--- to-do-app/src/main/java/com/codestates/todoapp/controller/TodoController.java
+++ to-do-app/src/main/java/com/codestates/todoapp/controller/TodoController.java
@@ -1,25 +1,32 @@
 package com.codestates.todoapp.controller;
 
+import com.codestates.todoapp.dto.TodoDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 
 @RestController
 @RequestMapping("/")
+@Validated
 public class TodoController {
 
     @PostMapping
-    public ResponseEntity postTodo() {
+    public ResponseEntity postTodo(@Valid @RequestBody TodoDto.Post requestBody) {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity patchTodo() {
+    public ResponseEntity patchTodo(@PathVariable("id") @Positive int id,
+                                    @RequestBody TodoDto.Patch requestBody) {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity getTodo() {
+    public ResponseEntity getTodo(@PathVariable("id") @Positive int id) {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
@@ -29,7 +36,7 @@ public class TodoController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteTodo() {
+    public ResponseEntity deleteTodo(@PathVariable("id") @Positive int id) {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
