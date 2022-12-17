370acb2b6f56cb7341fc0aa90b607ff898e4fc81
mason <masonlee7629@gmail.com>
2022-12-18 02:42:16 +0900

    [Fix] implemented dto

--- to-do-app/src/main/java/com/codestates/todoapp/dto/TodoDto.java
+++ to-do-app/src/main/java/com/codestates/todoapp/dto/TodoDto.java
@@ -1,4 +1,34 @@
 package com.codestates.todoapp.dto;
 
+import javax.validation.constraints.NotBlank;
+
 public class TodoDto {
+    public static class Post {
+        @NotBlank
+        private String title;
+
+        @NotBlank
+        private Integer todo_order;
+
+        @NotBlank
+        private Boolean completed;
+    }
+
+    public static class Patch {
+        private Integer id;
+        private String title;
+        private Integer todo_order;
+        private Boolean completed;
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+    }
+
+    public static class Response {
+        private Integer id;
+        private String title;
+        private Integer todo_order;
+        private Boolean completed;
+    }
 }
