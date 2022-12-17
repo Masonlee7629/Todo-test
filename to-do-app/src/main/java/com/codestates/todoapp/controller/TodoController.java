835d43b41b0c03870eab453450c18e83927e3fcb
mason <masonlee7629@gmail.com>
2022-12-18 01:58:36 +0900

    Fix parameter of response in controller

--- to-do-app/src/main/java/com/codestates/todoapp/controller/TodoController.java
+++ to-do-app/src/main/java/com/codestates/todoapp/controller/TodoController.java
@@ -1,5 +1,6 @@
 package com.codestates.todoapp.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,31 +10,31 @@ public class TodoController {
 
     @PostMapping
     public ResponseEntity postTodo() {
-        return new ResponseEntity<>();
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity patchTodo() {
-        return new ResponseEntity<>();
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity getTodo() {
-        return new ResponseEntity<>();
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity getTodos() {
-        return new ResponseEntity<>();
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity deleteTodo() {
-        return new ResponseEntity<>();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping
     public ResponseEntity deleteTodos() {
-        return new ResponseEntity<>();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
